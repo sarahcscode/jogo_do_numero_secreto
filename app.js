154b768a6fb89a35cca7bf74c0f012cde085c18b
sarahcscode <seuemail@exemplo.com>
2025-01-27 20:13:48 -0300

    teste

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let tentativas = 0;
 TextosIniciais();
 let ListaDeNumeros = [];
-let LimiteElemt = 40;
+let LimiteElemt = 20;
 let NumeroSecreto = Sorteio();
 document.getElementById('reiniciar').removeAttribute('disabled');
 
